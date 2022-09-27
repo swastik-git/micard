b7f8f7f98118b0d1c4a3fe5f5a5ed303808316f7
Swastik Dolas <swastikdolas2000@gmail.com>
2022-09-27 16:44:58 +0530

    done whitout avatar

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import Icon from "react-native-vector-icons/MaterialIcons"
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.insideContainer}>
 
+      <View style={styles.insideContainer}>
+        <View style={{ alignItems: 'center' }}>
+          <Text style={styles.name}>Swastik Dolas</Text>
+          <Text style={styles.position}>React-Native Developer</Text>
+        </View>
+
+        <Text style={{ alignSelf: "center" }}>___________________________</Text>
+        <View style={{ marginTop: 10 }} />
+
+        <Icon.Button
+          name="call"
+          backgroundColor="#DDD"
+          color="#000"
+          iconStyle={{ margin: 10 }}>
+          +91 7879996890
+        </Icon.Button>
+        <View style={{ marginTop: 10 }} />
+        <Icon.Button
+          name="email"
+          backgroundColor="#DDD"
+          color="#000"
+          iconStyle={{ margin: 10 }}>
+          Swastikdolas123@gmail.com
+        </Icon.Button>
       </View>
     </View>
   )
@@ -14,6 +38,7 @@ const App = () => {
 export default App;
 
 
+// styling 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -21,12 +46,22 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   insideContainer: {
+    flex: 1,
     flexDirection: 'column',
-    // alignSelf: "center",
-    // justifyContent: "center",
+    alignContent: "stretch",
+    justifyContent: "center",
     borderColor: "#000",
-    // padding: 20,
+    padding: 20,
     borderWidth: 1,
+  },
+  name: {
+    fontSize: 45,
+    fontFamily: "DancingScript-Bold"
+  },
+  position: {
+    fontSize: 18,
+    textTransform: "capitalize",
+    // fontFamily:"StyleScript-Regular"
   },
   styleForImage: {},
   styleForCard: {},
